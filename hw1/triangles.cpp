ed5a4f385441211e408fa6f030b62567e88a82fe
Aditya Kousik <adit267.kousik@gmail.com>
2015-02-13 08:43:43 -0700

    Separated solo triangle as new VAO+Toggle support

--- hw1/triangles.cpp
+++ hw1/triangles.cpp
@@ -17,21 +17,33 @@ using namespace std;
 #include "LoadShaders.h"
 
 //enum VAO_IDs { Triangles, NumVAOs };
-const GLuint Triangles = 0, NumVAOs = 1;
+const GLuint Triangles = 0, colorTriangle = 1, circle = 2, NumVAOs = 3;
 //enum Buffer_IDs { ArrayBuffer, NumBuffers };
 const GLuint ArrayBuffer = 0, colorBuffer = 1, NumBuffers = 2;
 //enum Attrib_IDs { vPosition = 0 };
 const GLuint vPosition = 0, cPosition = 1;
 
+const GLuint TWO_TRIANGLES = 0, COLOR_TRIANGLE = 1, CIRCLE = 2;
+
 GLuint VAOs[NumVAOs];
 GLuint Buffers[NumBuffers];
+GLuint trialBuf[NumBuffers];
 
-const GLuint NumVertices = 9;
+const GLuint NumVertices = 6;
 const GLuint NumTriangles = 2, NumColorTriangle = 1, NumCircle = 1;
 GLuint toggle[3] = { 1, 1, 0 };
 
 GLfloat r, g, b;
-GLfloat globalPositionArray[NumVertices][2], globalColorArray[NumVertices][3];
+GLfloat globalTrianglesPositionArray[NumVertices][2];
+GLfloat globalTrianglesColorArray[NumVertices][3];
+
+// Single color triangle
+GLfloat globalColorTrianglePositionArray[NumColorTriangle * 3][2];
+GLfloat globalColorTriangleColorArray[NumColorTriangle * 3][3];
+
+// Circle
+GLfloat **globalCirclePositionArray;
+GLfloat **globalCircleColorArray;
 
 void init();
 
@@ -51,9 +63,9 @@ void keyboard(unsigned char key, int x, int y)
 			{
 				switch (j)
 				{
-				case 0: globalColorArray[i][j] = r; break;
-				case 1: globalColorArray[i][j] = g; break;
-				case 2: globalColorArray[i][j] = b; break;
+				case 0: globalTrianglesColorArray[i][j] = r; break;
+				case 1: globalTrianglesColorArray[i][j] = g; break;
+				case 2: globalTrianglesColorArray[i][j] = b; break;
 				}
 			}
 		}
@@ -74,6 +86,21 @@ void keyboard(unsigned char key, int x, int y)
 	case 'q': case 'Q': case 27:
 		exit(EXIT_SUCCESS);
 		break;
+	case 'x': case 'X' :
+		toggle[TWO_TRIANGLES] = !toggle[TWO_TRIANGLES];
+		init();
+		glutPostRedisplay();
+		break;
+	case 'y': case 'Y':
+		toggle[COLOR_TRIANGLE] = !toggle[COLOR_TRIANGLE];
+		init();
+		glutPostRedisplay();
+		break;
+	case 'z': case 'Z':
+		toggle[CIRCLE] = !toggle[CIRCLE];
+		init();
+		glutPostRedisplay();
+		break;
 	default:
 		break;
 	}
@@ -84,29 +111,6 @@ void keyboard(unsigned char key, int x, int y)
 /////////////////////////////////////////////////////
 void init()
 {
-	glGenVertexArrays(NumVAOs, VAOs);
-	glBindVertexArray(VAOs[Triangles]);
-
-	GLfloat vertices[NumVertices][2], colors[NumVertices][3];
-
-	// Vertex positions
-	for (int i = 0; i < NumVertices; ++i)
-	{
-		for (int j = 0; j < 2; ++j)
-		{
-			vertices[i][j] = globalPositionArray[i][j];
-			colors[i][j] = globalColorArray[i][j];
-		}
-
-		colors[i][2] = globalColorArray[i][2];
-	}
-
-	glGenBuffers(NumBuffers, Buffers);
-	glBindBuffer(GL_ARRAY_BUFFER, Buffers[ArrayBuffer]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ARRAY_BUFFER, Buffers[colorBuffer]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
 
 	ShaderInfo  shaders[] = {
 		{ GL_VERTEX_SHADER, "triangles.vert" },
@@ -117,13 +121,75 @@ void init()
 	GLuint program = LoadShaders(shaders);
 	glUseProgram(program);
 
-	glBindBuffer(GL_ARRAY_BUFFER, Buffers[ArrayBuffer]);
-	glVertexAttribPointer(vPosition, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
-	glEnableVertexAttribArray(vPosition);
+	if (toggle[TWO_TRIANGLES] == 1)
+	{
+		glGenVertexArrays(NumVAOs, VAOs);
+		glBindVertexArray(VAOs[Triangles]);
 
-	glBindBuffer(GL_ARRAY_BUFFER, Buffers[colorBuffer]);
-	glVertexAttribPointer(cPosition, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
-	glEnableVertexAttribArray(cPosition);
+		GLfloat vertices[NumVertices][2], colors[NumVertices][3];
+
+		// Vertex positions
+		for (int i = 0; i < NumVertices; ++i)
+		{
+			for (int j = 0; j < 2; ++j)
+			{
+				vertices[i][j] = globalTrianglesPositionArray[i][j];
+				colors[i][j] = globalTrianglesColorArray[i][j];
+			}
+
+			colors[i][2] = globalTrianglesColorArray[i][2];
+		}
+
+		glGenBuffers(NumBuffers, Buffers);
+		glBindBuffer(GL_ARRAY_BUFFER, Buffers[ArrayBuffer]);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ARRAY_BUFFER, Buffers[colorBuffer]);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ARRAY_BUFFER, Buffers[ArrayBuffer]);
+		glVertexAttribPointer(vPosition, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+		glEnableVertexAttribArray(vPosition);
+
+		glBindBuffer(GL_ARRAY_BUFFER, Buffers[colorBuffer]);
+		glVertexAttribPointer(cPosition, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+		glEnableVertexAttribArray(cPosition);
+	}
+	/*  Trials  */
+
+	if (toggle[COLOR_TRIANGLE] == 1)
+	{
+		glBindVertexArray(VAOs[colorTriangle]);
+
+		GLfloat colorTrianglePos[3][2], colorTriangleCol[3][3];
+
+		// Vertex positions
+		for (int i = 0; i < 3; ++i)
+		{
+			for (int j = 0; j < 2; ++j)
+			{
+				colorTrianglePos[i][j] = globalColorTrianglePositionArray[i][j];
+				colorTriangleCol[i][j] = globalColorTriangleColorArray[i][j];
+			}
+
+			colorTriangleCol[i][2] = globalColorTriangleColorArray[i][2];
+		}
+
+		glGenBuffers(NumBuffers, trialBuf);
+		glBindBuffer(GL_ARRAY_BUFFER, trialBuf[0]);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(colorTrianglePos),
+			colorTrianglePos, GL_STATIC_DRAW);
+
+		glVertexAttribPointer(vPosition, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+		glEnableVertexAttribArray(vPosition);
+
+		glBindBuffer(GL_ARRAY_BUFFER, trialBuf[1]);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(colorTriangleCol),
+			colorTriangleCol, GL_STATIC_DRAW);
+
+		glVertexAttribPointer(cPosition, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+		glEnableVertexAttribArray(cPosition);
+	}
 }
 
 ////////////////////////////////////////////////////////////////////
@@ -132,9 +198,18 @@ void init()
 void display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
-	glBindVertexArray(VAOs[Triangles]);
-	glDrawArrays(GL_TRIANGLES, 0, NumVertices);
+
+	if (toggle[TWO_TRIANGLES] == 1)
+	{
+		glBindVertexArray(VAOs[Triangles]);
+		glDrawArrays(GL_TRIANGLES, 0, NumVertices);
+	}
 	//glDrawArrays( GL_LINES, 0, NumVertices );
+	if (toggle[COLOR_TRIANGLE] == 1)
+	{
+		glBindVertexArray(VAOs[colorTriangle]);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+	}
 
 	glFlush();
 }
@@ -179,38 +254,54 @@ int main(int argc, char* argv[])
 	//	cout << glGetStringi( GL_EXTENSIONS, i )  << endl;
 
 	// Initial values of the triangle color and position
-	GLfloat initVertices[NumVertices][2] = {
+	GLfloat initTrianglesVertices[NumVertices][2] = {
 		{ -0.90f, -0.9f },	// Triangle 1
 		{ 0.85f, -0.9f },
 		{ -0.90f, 0.85f },
 		{ 0.90f, -0.85f },	// Triangle 2
 		{ 0.90f, 0.90f },
-		{ -0.85f, 0.90f },
-		{ 0.0f, 0.5f },		// Center triangle
-		{ -0.5f, -0.5f },
-		{ 0.50f, -0.5f }
+		{ -0.85f, 0.90f }
 	};
 
-	GLfloat initColors[NumVertices][3] = {
+	GLfloat initTrianglesColors[NumVertices][3] = {
 		{ 0.0f, 0.0f, 1.0f }, // Triangle 1
 		{ 0.0f, 0.0f, 1.0f },
 		{ 0.0f, 0.0f, 1.0f },
 		{ 0.0f, 0.0f, 1.0f }, // Triangle 2
 		{ 0.0f, 0.0f, 1.0f },
 		{ 0.0f, 0.0f, 1.0f },
-		{ 1.0f, 0.0f, 0.0f }, // Center triangle
-		{ 0.0f, 0.0f, 1.0f },
-		{ 0.0f, 1.0f, 0.0f }
 	};
 
 	for (int i = 0; i < NumVertices; ++i)
 	{
 		for (int j = 0; j < 2; ++j)
 		{
-			globalPositionArray[i][j] = initVertices[i][j];
-			globalColorArray[i][j] = initColors[i][j];
+			globalTrianglesPositionArray[i][j] = initTrianglesVertices[i][j];
+			globalTrianglesColorArray[i][j] = initTrianglesColors[i][j];
 		}
-		globalColorArray[i][2] = initColors[i][2];
+		globalTrianglesColorArray[i][2] = initTrianglesColors[i][2];
+	}
+
+	GLfloat tmpVertices[3][2] = {
+		{ 0.0f, 0.6f },		// Center triangle
+		{ -0.6f, -0.6f },
+		{ 0.60f, -0.6f }
+	};
+
+	GLfloat tmpColors[3][3] = {
+		{ 1.0f, 0.0f, 0.0f }, // Center triangle
+		{ 0.0f, 0.0f, 1.0f },
+		{ 0.0f, 1.0f, 0.0f }
+	};
+
+	for (int i = 0; i < 3; ++i)
+	{
+		for (int j = 0; j < 2; ++j)
+		{
+			globalColorTrianglePositionArray[i][j] = tmpVertices[i][j];
+			globalColorTriangleColorArray[i][j] = tmpColors[i][j];
+		}
+		globalColorTriangleColorArray[i][2] = tmpColors[i][2];
 	}
 
 	init();
